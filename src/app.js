d73048e3180e106fce52a367548b151fe2fa5fc3
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-02-23 10:38:14 +0100

    finished

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+/*
+* File: app.js
+* Author: Vitovszki Tamás
+* Copyright: 2023, Vitovszki Tamás
+* Group: Szoft V
+* Date: 2023-02-23
+* Github: https://github.com/Tomasman05
+* Licenc: GNU GPL
+*/
 const radiusInput =document.querySelector("#radius")
 const heightInput =document.querySelector("#height")
 const areaInput =document.querySelector("#area")
