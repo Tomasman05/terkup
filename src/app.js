0a16e2cebca864d3a5bbb2e3fffd76b85b10b280
Vitovszki Tamas <lolozol80xd@gmail.com>
2023-02-24 08:56:17 +0100

    ongoing

--- src/app.js
+++ src/app.js
@@ -11,10 +11,11 @@ const radiusInput =document.querySelector("#radius")
 const heightInput =document.querySelector("#height")
 const areaInput =document.querySelector("#area")
 const calcButton =document.querySelector("#calcButton")
-
+const result =document.querySelector("#result")
 calcButton.addEventListener("click",()=>{
     const radius= Number (radiusInput.value)
     const height= Number (heightInput.value)
     let area= (1.0/3) * Math.pow(radius, 2)*Math.PI*height
     areaInput.value=area
+    result.style.display="block"
 })
